efd831290b29ed0bea18f86fd5b4c6d98dde4ac8
kacperdybala <luckarz07@gmail.com>
2023-10-06 22:11:10 +0200

    Calculator for beginners

--- kalkulator.py
+++ kalkulator.py
@@ -1,5 +1,22 @@
+# Enter
+
+name = input('Whats is your name? ')
+print('Hello,', name)
+
 # Numbers
-x = int(input('Podaj liczbe x '))
-y = int(input('Podaj liczbę y '))
-# wynik
-print(x + y)
+
+x = int(input('Provide number x '))
+y = int(input('Provide number y '))
+print( '1 - addition, 2 - subtraction, 3 - divide, 4 - multiplication ')
+action = int(input('Provide action '))
+
+# Solution
+
+if(action == 1):
+    print(x + y)
+if(action == 2):
+    print(x - y)
+if(action == 3):
+    print(x / y)
+if(action == 4):
+    print(x * y)
